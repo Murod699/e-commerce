bc2c812586d7ae3eebeb68f42f4012b822ff4943
Murodbek699 <murodbek.0338@gmail.com>
2023-08-30 00:02:03 +0500

    Autentifikatsiya va saralangan mahsulotlar

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FavoriteController extends Controller
+{
+
+    public function  __construct(){
+
+        $this->middleware('auth:sanctum');
+
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return auth()->user()->favorites()->paginate(20);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        auth()->user()->favorites()->attach($request->product_id);
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($favorite_id)
+    {
+        if(auth()->user()->hasFavorite($favorite_id)){
+            auth()->user()->favorites()->detach($favorite_id);
+
+            return response()->json(['success' => true]);
+        }
+        return response()->json(['success' => false, 'message' => 'Favorite does not exist in this user']);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -48,5 +49,16 @@ class User extends Authenticatable
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function favorites(): BelongsToMany
+    {
+
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function hasFavorite($favorite_id){
+
+        return $this->favorites()->where('product_id', $favorite_id)->exists();
     }
 }
